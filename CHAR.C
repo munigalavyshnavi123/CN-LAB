f1a5b9b9c7d40a2da8b5dc0f18f7afd41edbeaee
munigalavyshnavi123 <vyshnavimunigala4@gmail.com>
2024-10-01 15:03:32 +0530

    Add files via upload

--- CHAR.C
+++ CHAR.C
@@ -1,17 +1,16 @@
 #include<stdio.h>
-#include<conio.h>
 #include<string.h>
-void main()
+main()
 {
 char a[30],fs[50]=" ",t[3],sd,ed,x[3],s[3],d[3],y[3];
-int i,j,p=0,q=0;
+int i,j7;
 clrscr();
-printf("Enter characters to be stuffed:");
+printf("Enter the chatarter to be stuffed:");
 scanf("%s",a);
-printf("\n Enter a character that represents starting delimiter:");
-scanf(" %s",&sd);
-printf("\n Enter a character that represents ending delimiter:");
-scanf(" %c",&ed);
+printf("\n Ente a character that represents starting delimiter:");
+scanf("%c",&sd);
+printf("\n Enter a character that represents a ending delimiter:");
+scanf("%c",ed);
 x[0]=s[0]=s[1]=sd;
 x[1]=s[2]='\0';
 y[0]=d[0]=d[1]=ed;
@@ -29,6 +28,7 @@ else
 strcat(fs,t);
 }
 strcat(fs,y);
-printf("\n After stuffing:%s",fs);
+printf("\n After Stuffing : %s",fs);
 getch();
+return 0;
 }
